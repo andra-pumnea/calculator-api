cf182117a5f0a4b9cc697e568f2cd48685a350fa
Andrada Pumnea <andrada.pumnea@futurice.com>
2020-05-11 09:20:21 +0200

    remove redundant function

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,9 @@ import {calculate} from './evaluate';
 const app = express();
 const port = 5050;
 
-function add(x:number, y:number): number {
-  return x + y
-}
-
-function sub(x:number, y:number): number {
-  return x - y
-}
-
-function mul(x:number, y:number): number {
-  return x * y
-}
-
-function div(x:number, y:number): number {
-  return x / y
+function decodeQuery(encoded: string): string {
+  const decoded = Buffer.from(encoded, 'base64').toString('ascii')
+  return decoded;
 }
  
 app.get('/calculus', (request, response) => {
@@ -27,10 +16,6 @@ app.get('/calculus', (request, response) => {
   response.send(calculated_expression[0]);
 });
 
-function decodeQuery(encoded: string): string {
-  const decoded = Buffer.from(encoded, 'base64').toString('ascii')
-  return decoded;
-}
 
 app.listen( port, () => {
   console.log( `server started at http://localhost:${ port }/calculus?query=MiAqICgyMy8oMzMpKS0gMjMgKiAoMjMp` );
